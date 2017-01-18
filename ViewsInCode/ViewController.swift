973afef977a633cd78aaef98818d3c0ed6766531
Mirczenzo <mateusz.mirkowski@visentcoders.com>
2017-01-18 20:40:26 +0100

    Initial Commit

---
+++ ViewsInCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewsInCode
+//
+//  Created by grzesiek on 18/01/2017.
+//  Copyright © 2017 Mateusz Mirkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
